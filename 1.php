469274364492c64dfffbccf1a994979035985987
lht2013 <2357071@qq.com>
2013-12-12 10:36:04 +0800

    1.php2

--- 1.php
+++ 1.php
@@ -1,17 +1,8 @@
-<?php
+﻿<?php
 /**
  *前台入口文件
  */
-define('ALLOW', TRUE);
-define('FILE_ROOT', dirname(__FILE__));
-require './config/config_global.php';
-require './source/function/function_global.php';
-require './source/class/class_router.php';
-require './source/class/class.db_mysql.php';
-require './source/include/checkform.php';
-require './source/class/class_base.php';
-require './source/class/class_checker.php';
-$class_router=new class_router('action',$_GET["action"]);
+1
 
 
 ?>
